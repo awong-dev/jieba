a0d32ffebba11dccd04221ff7ec3ea0580dfdcaf
Albert J. Wong <awong.dev@gmail.com>
2024-03-31 21:40:24 -0700

    Reformat rust

--- native/rustler_jieba/src/lib.rs
+++ native/rustler_jieba/src/lib.rs
@@ -1,7 +1,9 @@
 use lazy_static::lazy_static;
 
-use jieba_rs::{Jieba, Error as JiebaError, KeywordExtract, TFIDF, TextRank, TokenizeMode};
-use rustler::{types::tuple, Encoder, Env, Error as RustlerError, NifStruct, NifUnitEnum, ResourceArc, Term};
+use jieba_rs::{Error as JiebaError, Jieba, KeywordExtract, TextRank, TokenizeMode, TFIDF};
+use rustler::{
+    types::tuple, Encoder, Env, Error as RustlerError, NifStruct, NifUnitEnum, ResourceArc, Term,
+};
 
 use std::fs::File;
 use std::io::BufReader;
@@ -26,8 +28,8 @@ mod atoms {
 
 #[derive(NifUnitEnum)]
 enum TokenizeEnum {
-   Default,
-   Search,
+    Default,
+    Search,
 }
 
 pub struct JiebaResource {
@@ -39,7 +41,7 @@ pub struct JiebaResource {
 struct ElixirJieba {
     pub use_default: bool,
     pub dict_paths: Vec<String>,
-    pub native: ResourceArc<JiebaResource>
+    pub native: ResourceArc<JiebaResource>,
 }
 
 #[derive(NifStruct)]
@@ -83,7 +85,7 @@ fn io_error_to_rustler_error(err: IoError) -> RustlerError {
 fn jieba_error_to_rustler_error(jieba_err: JiebaError) -> RustlerError {
     match jieba_err {
         JiebaError::Io(io_err) => io_error_to_rustler_error(io_err),
-        JiebaError::InvalidDictEntry(entry) => RustlerError::Term(Box::new(entry))
+        JiebaError::InvalidDictEntry(entry) => RustlerError::Term(Box::new(entry)),
     }
 }
 
@@ -94,7 +96,7 @@ fn native_new() -> ElixirJieba {
         dict_paths: Vec::new(),
         native: ResourceArc::new(JiebaResource {
             jieba_rs: Mutex::new(Jieba::new()),
-        })
+        }),
     }
 }
 
@@ -105,7 +107,7 @@ fn native_empty() -> ElixirJieba {
         dict_paths: Vec::new(),
         native: ResourceArc::new(JiebaResource {
             jieba_rs: Mutex::new(Jieba::empty()),
-        })
+        }),
     }
 }
 
@@ -116,19 +118,25 @@ fn clone(jieba: ElixirJieba) -> ElixirJieba {
         use_default: jieba.use_default,
         dict_paths: jieba.dict_paths.clone(),
         native: ResourceArc::new(JiebaResource {
-            jieba_rs: Mutex::new(jieba_rs.clone())
-        })
+            jieba_rs: Mutex::new(jieba_rs.clone()),
+        }),
     }
 }
 
 #[rustler::nif]
-fn load_dict<'a>(env: Env<'a>, in_jieba: ElixirJieba, dict_path: String) -> Result<Term<'a>, RustlerError> {
+fn load_dict<'a>(
+    env: Env<'a>,
+    in_jieba: ElixirJieba,
+    dict_path: String,
+) -> Result<Term<'a>, RustlerError> {
     let file = File::open(&dict_path).map_err(io_error_to_rustler_error)?;
     let mut jieba = in_jieba;
     {
         let jieba_rs = &mut jieba.native.jieba_rs.lock().unwrap();
         let mut reader = BufReader::new(file);
-        jieba_rs.load_dict(&mut reader).map_err(jieba_error_to_rustler_error)?;
+        jieba_rs
+            .load_dict(&mut reader)
+            .map_err(jieba_error_to_rustler_error)?;
         jieba.dict_paths.push(dict_path);
     }
     let ok_atom_term = atoms::ok().encode(env);
@@ -137,20 +145,30 @@ fn load_dict<'a>(env: Env<'a>, in_jieba: ElixirJieba, dict_path: String) -> Resu
 
 #[rustler::nif]
 fn suggest_freq(jieba: ElixirJieba, segment: String) -> usize {
-    jieba.native.jieba_rs.lock().unwrap()
-        .suggest_freq(&segment)
+    jieba.native.jieba_rs.lock().unwrap().suggest_freq(&segment)
 }
 
 #[rustler::nif]
 fn add_word(jieba: ElixirJieba, word: String, freq: Option<usize>, new_tag: Option<&str>) -> usize {
-    jieba.native.jieba_rs.lock().unwrap()
+    jieba
+        .native
+        .jieba_rs
+        .lock()
+        .unwrap()
         .add_word(&word, freq, new_tag)
 }
 
 #[rustler::nif]
 fn native_cut(jieba: ElixirJieba, sentence: String, hmm: bool) -> Vec<String> {
-    jieba.native.jieba_rs.lock().unwrap()
-        .cut(&sentence, hmm).into_iter().map(|s| s.to_string()).collect()
+    jieba
+        .native
+        .jieba_rs
+        .lock()
+        .unwrap()
+        .cut(&sentence, hmm)
+        .into_iter()
+        .map(|s| s.to_string())
+        .collect()
 }
 
 lazy_static! {
@@ -159,65 +177,115 @@ lazy_static! {
 
 #[rustler::nif]
 fn native_static_cut(sentence: String) -> Vec<String> {
-    STATIC_JIEBA.cut(&sentence, false).into_iter().map(|s| s.to_string()).collect()
+    STATIC_JIEBA
+        .cut(&sentence, false)
+        .into_iter()
+        .map(|s| s.to_string())
+        .collect()
 }
 
 #[rustler::nif]
 fn cut_all(jieba: ElixirJieba, sentence: String) -> Vec<String> {
-    jieba.native.jieba_rs.lock().unwrap()
-        .cut_all(&sentence).into_iter().map(|s| s.to_string()).collect()
+    jieba
+        .native
+        .jieba_rs
+        .lock()
+        .unwrap()
+        .cut_all(&sentence)
+        .into_iter()
+        .map(|s| s.to_string())
+        .collect()
 }
 
 #[rustler::nif]
 fn cut_for_search(jieba: ElixirJieba, sentence: String, hmm: bool) -> Vec<String> {
-    jieba.native.jieba_rs.lock().unwrap()
-        .cut_for_search(&sentence, hmm).into_iter().map(|s| s.to_string()).collect()
+    jieba
+        .native
+        .jieba_rs
+        .lock()
+        .unwrap()
+        .cut_for_search(&sentence, hmm)
+        .into_iter()
+        .map(|s| s.to_string())
+        .collect()
 }
 
 #[rustler::nif]
-fn tokenize(jieba: ElixirJieba, sentence: String, mode: TokenizeEnum, hmm: bool) -> Vec<JiebaToken> {
-    jieba.native.jieba_rs.lock().unwrap()
-        .tokenize(&sentence,
-                   match mode {
-                     TokenizeEnum::Default => TokenizeMode::Default,
-                     TokenizeEnum::Search => TokenizeMode::Search
-                   },
-                   hmm)
+fn tokenize(
+    jieba: ElixirJieba,
+    sentence: String,
+    mode: TokenizeEnum,
+    hmm: bool,
+) -> Vec<JiebaToken> {
+    jieba
+        .native
+        .jieba_rs
+        .lock()
+        .unwrap()
+        .tokenize(
+            &sentence,
+            match mode {
+                TokenizeEnum::Default => TokenizeMode::Default,
+                TokenizeEnum::Search => TokenizeMode::Search,
+            },
+            hmm,
+        )
         .into_iter()
-        .map(|t| JiebaToken{ word: t.word.to_string(), start: t.start } )
+        .map(|t| JiebaToken {
+            word: t.word.to_string(),
+            start: t.start,
+        })
         .collect()
 }
 
 #[rustler::nif]
 fn tag(jieba: ElixirJieba, sentence: String, hmm: bool) -> Vec<JiebaTag> {
-    jieba.native.jieba_rs.lock().unwrap()
+    jieba
+        .native
+        .jieba_rs
+        .lock()
+        .unwrap()
         .tag(&sentence, hmm)
         .into_iter()
-        .map(|t| JiebaTag{ word: t.word.to_string(), tag: t.tag.to_string() })
+        .map(|t| JiebaTag {
+            word: t.word.to_string(),
+            tag: t.tag.to_string(),
+        })
         .collect()
 }
 
 #[rustler::nif]
-fn tfidf_extract_tags<'a>(env: Env<'a>, jieba: ElixirJieba, sentence: String, top_k: usize,
-        allowed_pos: Vec<String>, tfidf_dict_path: String,
-        stop_words: Vec<String>) -> Result<Term<'a>, RustlerError> {
-
+fn tfidf_extract_tags<'a>(
+    env: Env<'a>,
+    jieba: ElixirJieba,
+    sentence: String,
+    top_k: usize,
+    allowed_pos: Vec<String>,
+    tfidf_dict_path: String,
+    stop_words: Vec<String>,
+) -> Result<Term<'a>, RustlerError> {
     let jieba = jieba.native.jieba_rs.lock().unwrap();
     let mut keyword_extractor = TFIDF::new_with_jieba(&jieba);
 
     if !tfidf_dict_path.is_empty() {
         let file = File::open(&tfidf_dict_path).map_err(io_error_to_rustler_error)?;
         let mut reader = BufReader::new(file);
-        keyword_extractor.load_dict(&mut reader).map_err(io_error_to_rustler_error)?;
+        keyword_extractor
+            .load_dict(&mut reader)
+            .map_err(io_error_to_rustler_error)?;
     }
 
     for word in stop_words.into_iter() {
-       keyword_extractor.add_stop_word(word);
+        keyword_extractor.add_stop_word(word);
     }
 
-    let result : Vec<JiebaKeyword> = keyword_extractor.extract_tags(&sentence, top_k, allowed_pos)
+    let result: Vec<JiebaKeyword> = keyword_extractor
+        .extract_tags(&sentence, top_k, allowed_pos)
         .into_iter()
-        .map(|e| JiebaKeyword{ keyword: e.keyword.to_string(), weight: e.weight })
+        .map(|e| JiebaKeyword {
+            keyword: e.keyword.to_string(),
+            weight: e.weight,
+        })
         .collect();
 
     let ok_atom_term = atoms::ok().encode(env);
@@ -225,19 +293,28 @@ fn tfidf_extract_tags<'a>(env: Env<'a>, jieba: ElixirJieba, sentence: String, to
 }
 
 #[rustler::nif]
-fn textrank_extract_tags<'a>(env: Env<'a>, jieba: ElixirJieba, sentence: String, top_k: usize,
-        allowed_pos: Vec<String>, stop_words: Vec<String>) -> Result<Term<'a>, RustlerError> {
-
+fn textrank_extract_tags<'a>(
+    env: Env<'a>,
+    jieba: ElixirJieba,
+    sentence: String,
+    top_k: usize,
+    allowed_pos: Vec<String>,
+    stop_words: Vec<String>,
+) -> Result<Term<'a>, RustlerError> {
     let jieba = jieba.native.jieba_rs.lock().unwrap();
     let mut keyword_extractor = TextRank::new_with_jieba(&jieba);
 
     for word in stop_words.into_iter() {
-       keyword_extractor.add_stop_word(word);
+        keyword_extractor.add_stop_word(word);
     }
 
-    let result : Vec<JiebaKeyword> = keyword_extractor.extract_tags(&sentence, top_k, allowed_pos)
+    let result: Vec<JiebaKeyword> = keyword_extractor
+        .extract_tags(&sentence, top_k, allowed_pos)
         .into_iter()
-        .map(|e| JiebaKeyword{ keyword: e.keyword.to_string(), weight: e.weight })
+        .map(|e| JiebaKeyword {
+            keyword: e.keyword.to_string(),
+            weight: e.weight,
+        })
         .collect();
 
     let ok_atom_term = atoms::ok().encode(env);
@@ -246,7 +323,21 @@ fn textrank_extract_tags<'a>(env: Env<'a>, jieba: ElixirJieba, sentence: String,
 
 rustler::init!(
     "Elixir.Jieba",
-    [native_new, native_empty, clone, load_dict, suggest_freq, add_word, native_cut,
-    native_static_cut, cut_all, cut_for_search, tokenize, tag, tfidf_extract_tags,
-    textrank_extract_tags],
-    load = on_load);
+    [
+        native_new,
+        native_empty,
+        clone,
+        load_dict,
+        suggest_freq,
+        add_word,
+        native_cut,
+        native_static_cut,
+        cut_all,
+        cut_for_search,
+        tokenize,
+        tag,
+        tfidf_extract_tags,
+        textrank_extract_tags
+    ],
+    load = on_load
+);
