2ea4fae73789cee5361851f90dce8bdd641317e8
Albert J. Wong <awong.dev@gmail.com>
2024-04-01 23:16:27 -0700

    Add test data creator for segmenter.

---
+++ corpus/create_testdata.py
@@ -0,0 +1,30 @@
+#!python
+
+import pycantonese
+import json
+
+corpora = [
+    "https://childes.talkbank.org/data/Biling/CHCC.zip",
+    "https://childes.talkbank.org/data/Biling/Guthrie.zip",
+    "https://childes.talkbank.org/data/Chinese/Cantonese/HKU.zip",
+    "https://childes.talkbank.org/data/Chinese/Cantonese/LeeWongLeung.zip",
+    "https://childes.talkbank.org/data/Biling/Leo.zip",
+    "https://phonbank.talkbank.org/data/Chinese/Cantonese/PaidoCantonese.zip",
+    "https://childes.talkbank.org/data/Biling/YipMatthews.zip"
+]
+
+def main():
+    hkcancor = pycantonese.hkcancor()
+    utt_by_files = hkcancor.utterances(by_files=True)
+
+    segmentation_data = []
+    for f in utt_by_files:
+      for utt in f:
+          segments = [ token.word for token in utt.tokens ]
+          text = ''.join(segments)
+          segmentation_data.append([text,segments])
+    with open('../test/data/hkcancor_segmentation_testdata.json', 'w') as f:
+        json.dump(segmentation_data, f)
+
+if __name__ == '__main__':
+    main()
